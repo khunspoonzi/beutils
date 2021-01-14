cca714f428cb78b526e027ee3095d8118c126e2a
khunspoonzi <khunspoonzi@gmail.com>
2021-01-14 15:39:56 +0700

    Improve meta object in pagination

--- pagination.py
+++ pagination.py
@@ -22,3 +22,17 @@ class DefaultPagination(DynamicPageNumberPagination):
     max_page_size = 100
     page_query_param = "page"
     page_size_query_param = "page_size"
+
+    # ---------------------------------------------------------------------------------
+    # GET PAGE METADATA
+    # ---------------------------------------------------------------------------------
+
+    def get_page_metadata(self):
+        """ Constructs the meta data for the paginated response """
+
+        return {
+            "total_results": self.page.paginator.count,
+            "total_pages": self.page.paginator.num_pages,
+            self.page_query_param: self.page.number,
+            self.page_size_query_param: self.get_page_size(self.request),
+        }
